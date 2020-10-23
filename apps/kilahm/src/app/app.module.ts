f4d78ef33ab039b5420f2444989f43f94cdb0b5d
Isaac Riceweber <brotchen@gmail.com>
2020-10-23 12:50:54 -0700

    fix: include missed file from PR #18

--- apps/kilahm/src/app/app.module.ts
+++ apps/kilahm/src/app/app.module.ts
@@ -1,27 +1,20 @@
-import { Location } from '@angular/common';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { followDeepLink } from './initialization';
 import { ResumeComponent } from './resume/resume.component';
 import { routes } from './routes';
 @NgModule({
   declarations: [AppComponent, ResumeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, { initialNavigation: 'disabled', enableTracing:  !environment.production}),
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'disabled',
+      enableTracing: !environment.production,
+    }),
   ],
-  providers: [
-    Location,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: followDeepLink,
-      multi: true,
-      deps: [Location, Router],
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
