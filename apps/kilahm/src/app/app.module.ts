c2b8ca74a04fe59ef4dd89e04a6e4e954b637bcb
Isaac Riceweber <brotchen@gmail.com>
2020-10-23 12:22:50 -0700

    feat: allow deep links
    Provide a custom 404 page that uses the history api to redirect the browser to the index page with a
    url hash to the path requested. Angular bootstrap then picks up on the has in the url to perform the
    initial Angular router navigation.
    
    fix #14

--- apps/kilahm/src/app/app.module.ts
+++ apps/kilahm/src/app/app.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Location } from '@angular/common';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { followDeepLink } from './initialization';
 import { ResumeComponent } from './resume/resume.component';
 import { routes } from './routes';
-
 @NgModule({
   declarations: [AppComponent, ResumeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'disabled', enableTracing:  !environment.production}),
   ],
-  providers: [],
+  providers: [
+    Location,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: followDeepLink,
+      multi: true,
+      deps: [Location, Router],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
